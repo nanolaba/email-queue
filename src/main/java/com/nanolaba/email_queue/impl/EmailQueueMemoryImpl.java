2846624195a5d4737452b66cc66ee4b61b326f36
alexander.andriishin <alexander.andriishin@gmail.com>
2014-04-30 19:26:36 +0400

    .

--- src/main/java/com/nanolaba/email_queue/impl/EmailQueueMemoryImpl.java
+++ src/main/java/com/nanolaba/email_queue/impl/EmailQueueMemoryImpl.java
@@ -3,30 +3,33 @@ package com.nanolaba.email_queue.impl;
 import com.nanolaba.email_queue.EmailMessage;
 import com.nanolaba.email_queue.SendEmailException;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
 public class EmailQueueMemoryImpl extends AbstractQueue {
 
-    private List<EmailMessage> messages = new LinkedList<EmailMessage>();
+    private final Collection<EmailMessage> messages = new LinkedList<EmailMessage>();
 
     @Override
     public void add(EmailMessage emailMessage) throws SendEmailException {
-        messages.add(emailMessage);
+        synchronized (messages) {
+            messages.add(emailMessage);
+        }
     }
 
     @Override
-    public synchronized void flush() throws SendEmailException {
+    public void flush() throws SendEmailException {
+        synchronized (messages) {
+            Iterator<EmailMessage> iterator = messages.iterator();
 
-        Iterator<EmailMessage> iterator = messages.iterator();
-
-        while (iterator.hasNext()) {
-            try {
-                EmailMessage message = iterator.next();
-                send(message);
-            } finally {
-                iterator.remove();
+            while (iterator.hasNext()) {
+                try {
+                    EmailMessage message = iterator.next();
+                    send(message);
+                } finally {
+                    iterator.remove();
+                }
             }
         }
     }
